113edb3599f2d0c4e3520ba0a943903f180296c0
BuenVia <matthewclifford@hotmail.co.uk>
2024-07-21 13:58:48 +0100

    update

--- front_end/src/components/Sidebar.jsx
+++ front_end/src/components/Sidebar.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
+
 const Sidebar = () => {
-    return (
+
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+      const url = "http://127.0.0.1:8000/api/blogs/"
+      const getBlogs = async () => {
+          try {
+              await axios.get(url).then(res => setBlogs(res.data))
+          } catch (error) {
+              console.error(error);
+          }
+      }
+      getBlogs()
+  }, [])
+
+
+  return (
       <aside className="sidebar">
         <h2>Latest Articles</h2>
         <ul>
+          
+          <h1>WRITE CODE TO SHOW ARTICLES HERE</h1>
+          <h3>wRITE CODE TO SHOW TESTS ON THE MAIN SECTION</h3>
+          
           <li><a href="#article1">Article 1</a></li>
           <li><a href="#article2">Article 2</a></li>
           <li><a href="#article3">Article 3</a></li>
